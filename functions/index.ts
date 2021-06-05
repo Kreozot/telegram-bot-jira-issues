91a8bbd55bc3c67234e27f68e5a359910201dbac
Kreozot <zzxyyz@gmail.com>
2021-06-05 10:18:00 +0300

    Test with JIRA and links

--- functions/index.ts
+++ functions/index.ts
@@ -1,9 +1,11 @@
 import { Handler, APIGatewayEvent } from 'aws-lambda';
 import { Telegraf, Context } from 'telegraf';
+import dotenv from 'dotenv';
 
 import startAction from './start';
 import inlineAction from './inline';
 
+dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx: Context) => {
--- functions/inline.ts
+++ functions/inline.ts
@@ -1,7 +1,27 @@
 import { Context } from 'telegraf';
+import JiraApi from 'jira-client';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const askJira = async () => {
+  const jira = new JiraApi({
+    protocol: 'https',
+    host: 'l2u.atlassian.net',
+    username: process.env.JIRA_USERNAME,
+    password: process.env.JIRA_API_TOKEN,
+    apiVersion: '2',
+    strictSSL: true
+  });
+
+  const issue = await jira.findIssue('DES-30');
+  console.log(issue);
+}
+
+// askJira();
 
 export default async (ctx: Context) => {
-  const search = (ctx.inlineQuery.query || "");
+  const search = (ctx.inlineQuery && ctx.inlineQuery.query || "");
   if (search === "") {
     return;
   }
@@ -11,9 +31,9 @@ export default async (ctx: Context) => {
     title: 'Ну давай, попробуй',
     type: 'article',
     input_message_content: {
-      message_text: search.replace(/пыщь/gm, 'попячсо'),
+      message_text: search.replace(/пыщь/gm, '[попячсо](http://upyachka.ru)'),
       parse_mode: 'HTML'
     }
   }];
-  return ctx.answerInlineQuery(answer);
+  return ctx.answerInlineQuery(answer as any);
 };
